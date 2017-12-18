d93c931bd46dd1d3cfaac7673bc59e4a5de97a84
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2017-12-18 18:02:21 +0100

    Add build support for mirage

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -6,10 +6,20 @@
 open Topkg
 open Ocb_stubblr_topkg
 
+let xen  = Conf.(key "xen" bool ~absent:false
+                   ~doc:"Build Mirage/Xen support.")
+let fs   = Conf.(key "freestanding" bool ~absent:false
+                   ~doc:"Build Mirage/Solo5 support.")
+
+let build = Pkg.build ~cmd ()
+
 let () =
-  Pkg.describe "chacha" @@ fun _c ->
+  Pkg.describe "chacha" ~build @@ fun c ->
+  let xen  = Conf.value c xen
+  and fs   = Conf.value c fs in
   Ok [
     Pkg.clib "libchacha-core.clib";
-    Pkg.mllib "chacha.mllib";
-    Pkg.test "chacha_tests"
+    Pkg.mllib ~api:["Chacha"] "chacha.mllib";
+    Pkg.test "chacha_tests";
+    mirage ~xen ~fs "libchacha.clib";
   ]
